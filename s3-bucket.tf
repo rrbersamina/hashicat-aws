a13bc9a9d4690706d7316390b51687dc5394ac7e
rrbersamina <rr.bersamina@ncs.com.sg>
2023-05-08 09:40:41 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/rrbersamina/s3-bucket/aws"
+  source  = "app.terraform.io/ncs-rochelle/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = "rochellebersamina"
- }
+}
